0e7a0fa53d918d8b1a30a26ce4a4e70cd5605544
laurenvast <wzrtoby@gmail.com>
2019-11-08 22:29:20 -0500

    installed

---
+++ js/cube.js
@@ -0,0 +1,67 @@
+var cube = function(p){
+
+  var a = 0, d = 1;
+
+  p.setup = function () {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    // p.canvas.id('cube');
+  }
+
+  p.windowResized = function() {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  }
+
+  p.draw = function() {
+    p.background(255);
+
+    var x, y, l, m, mx, my, x1, x2, x3, x4, y1, y2, y3, y4, y5, y6;
+
+    x = p.width / 7 * 5;
+    y = p.height / 2;
+    l = (p.height + p.width) / 2 / 4;
+
+    //find the distants between mouse and mid-point
+    mx = (p.mouseX - p.width / 2) / 3;
+    my = (p.mouseY - p.height / 2) / 2;
+
+    m = Math.sqrt(l * l - l / 2 * l / 2);
+    x1 = x - m;
+    x2 = x - mx - a;
+    x3 = x + m;
+    x4 = x + mx + a;
+    y1 = y - l + my;
+    y2 = y - l / 2;
+    y3 = y - my;
+    y4 = y + l / 2;
+    y5 = y3 + l;
+    y6 = y + my;
+    // fill(255, 0);
+
+    p.stroke(212, 72, 37);
+    p.strokeWeight(l/25);
+
+    p.line(x1, y2, x4, y3);
+    p.line(x4, y3, x4, y5);
+    p.line(x4, y5, x1, y4);
+    p.line(x1, y4, x1, y2);
+    p.line(x1, y2, x2, y1);
+    p.line(x2, y1, x2, y6);
+    p.line(x2, y6, x1, y4);
+    p.line(x3, y2, x2, y1);
+    p.line(x2, y6, x3, y4);
+    p.line(x3, y4, x3, y2);
+    p.line(x4, y5, x3, y4);
+    p.line(x3, y2, x4, y3);
+    
+    a += d;
+
+    if (a >= (p.height + p.width) / 2/2) {
+      d = -d;
+    }
+    if (a <= 0) {
+      d = -d;
+    }
+  }
+}
+
+var myp5 = new p5(cube, 'cube');
---
+++ js/scroll.js
@@ -0,0 +1,86 @@
+var navbarHeight = $('header').height();
+
+$(window).load(function () {
+    "use strict";
+    // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+})
+
+$(document).ready(function () {
+    "use strict";
+
+    $('div.portfolio-padding').css("padding-top", navbarHeight+50);
+
+
+    // Menu opacity
+    if ($(window).scrollTop() > 80) {
+        $(".navbar-fixed-top").addClass("scroll-nav");
+    } else {
+        $(".navbar-fixed-top").removeClass("scroll-nav");
+    }
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 80) {
+            $(".navbar-fixed-top").addClass("scroll-nav");
+        } else {
+            $(".navbar-fixed-top").removeClass("scroll-nav");
+        }
+    });
+
+
+
+
+
+// AOS
+    AOS.init({
+        duration: 1200,
+        once: true,
+        disable: 'mobile'
+    });
+
+})
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').height();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+        $('header.nav-up').css( "top", navbarHeight*-1 );
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+             $('header').css( "top", "" );
+        }
+    }
+    
+    lastScrollTop = st;
+}
